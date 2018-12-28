83ca540043a4f4beb4dfa8337323502be445ac21
blinker1104 <bjpark1104@ucdavis.edu>
2018-12-27 21:32:34 -0800

    Updated basic layout - Joon

--- app/src/main/java/com/android/ecs160/gpacalc/MainActivity.java
+++ app/src/main/java/com/android/ecs160/gpacalc/MainActivity.java
@@ -2,8 +2,12 @@ package com.android.ecs160.gpacalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +24,28 @@ public class MainActivity extends AppCompatActivity {
 
         btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
+
                 Toast.makeText(getApplicationContext(), "Application", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        EditText editText1  = (EditText) findViewById(R.id.editText1);
+        editText1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //Not using
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                final String printVal = (EditText)findViewById(R.id.editText1).getText();
+                Toast.makeText(getApplicationContext(), "Value = " + printVal, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
